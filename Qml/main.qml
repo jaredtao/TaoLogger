dc0de4c139165ccfa2b59cad77a62535a4c97b6c
jiawentao <wentaojia2014@163.com>
2019-06-21 09:18:10 +0800

    更新多线程测试，不卡UI

--- Qml/main.qml
+++ Qml/main.qml
@@ -1,8 +1,11 @@
-import QtQuick 2.0
+﻿import QtQuick 2.0
 import QtQuick.Controls 2.0
 Item {
     Text {
-        text: "TaoLogger"
+        text: "TaoLogger " + fpsItem.fps + " fps"
         anchors.centerIn: parent
+    }
+    FPSItem {
+        id: fpsItem
     }
 }
--- Src/main.cpp
+++ Src/main.cpp
@@ -13,7 +13,7 @@
 
 using namespace  Logger;
 using namespace std;
-const int N = 8;
+const int N = 17;
 
 //原子计数器。不要用普通的int，多线程情况下不准确。
 static atomic_int gCount(0);
@@ -25,11 +25,8 @@ static QString currentThreadId()
     return QString::fromStdString(ss.str());
 }
 //输出log的函数，随便打印一点信息
-static void showSomeLogger(int index)
+static void showSomeLogger()
 {
-    //总共8个线程，0号线程睡8毫秒，1号线程睡7毫秒，依次类推
-    //8个线程同时睡醒。
-    this_thread::sleep_for(chrono::milliseconds(N - index));
     //在文件本身的编码是utf-8的前提下，以下三种方式都可以直接输出中文。
     //u8是c++11标准支持的字符串字面量写法，可以参考https://zh.cppreference.com/w/cpp/language/string_literal
     //QStringLiteral是Qt特有的宏，用来在编译期生成字符串字面量
@@ -38,15 +35,32 @@ static void showSomeLogger(int index)
     {
         gCount++;
         int count = gCount;
-        LOG_DEBUG << currentThreadId() << u8"山有木兮木有枝，心悦君兮君不知。";
-        LOG_DEBUG << currentThreadId() << QStringLiteral("黄河远上白云间，一片孤城万仞山。");
-        LOG_DEBUG << currentThreadId() << QString::fromLocal8Bit("人生若只如初见，何事秋风悲画扇。");
-        LOG_INFO << currentThreadId() << u8"玲珑骰子安红豆，入骨相思知不知。";
-        LOG_WARN << currentThreadId() << u8"此情可待成追忆，只是当时已惘然。";
+//        LOG_DEBUG << currentThreadId() << u8"山有木兮木有枝，心悦君兮君不知。";
+//        LOG_DEBUG << currentThreadId() << QStringLiteral("黄河远上白云间，一片孤城万仞山。");
+//        LOG_DEBUG << currentThreadId() << QString::fromLocal8Bit("人生若只如初见，何事秋风悲画扇。");
+//        LOG_INFO << currentThreadId() << u8"玲珑骰子安红豆，入骨相思知不知。";
+//        LOG_WARN << currentThreadId() << u8"此情可待成追忆，只是当时已惘然。";
         LOG_CRIT << currentThreadId() << u8"严重的事情发生了，股票跌了!" << count;
 
         qApp->processEvents();
     }
+}
+static void logThread()
+{
+    vector<std::thread> threads;
+
+    //创建N个线程
+    for (unsigned long long i = 0; i < N; ++i) {
+        threads.emplace_back(thread(showSomeLogger));
+    }
+
+    //join,等线程结束。
+    for (unsigned long long i = 0; i < N; ++i)
+    {
+        threads[i].join();
+    }
+    LOG_INFO << currentThreadId() << u8"故事到这就结束了，总共输出log" << gCount;
+
 }
 int main(int argc, char *argv[])
 {
@@ -60,22 +74,10 @@ int main(int argc, char *argv[])
     view.resize(800, 600);
     view.setSource(QUrl(QStringLiteral("qrc:/Qml/main.qml")));
     view.show();
-    vector<std::thread> threads;
-    //创建N个线程
-    for (unsigned long long i = 0; i < N; ++i) {
-        threads.emplace_back(thread(showSomeLogger, i));
-    }
-    //延迟join,让UI的事件循环先跑起来
-    QTimer::singleShot(100, [&](){
-        //启动N个线程
-        for (unsigned long long i = 0; i < N; ++i)
-        {
-            threads[i].join();
-        }
+
+    QTimer::singleShot(2000, [&](){
+        std::thread logT(logThread);
+        logT.detach();
     });
-
-
-
-    LOG_INFO << currentThreadId() << u8"故事到这就结束了，总共输出log" << gCount;
     app.exec();
 }
